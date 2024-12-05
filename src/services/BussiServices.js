608b4a137b90d4fc43ee3442ee09d506d7e27166
Katherineoliva <104865553+Katherineoliva@users.noreply.github.com>
2024-12-04 23:08:20 -0600

    Update BussiServices.js

--- src/services/BussiServices.js
+++ src/services/BussiServices.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const getTarjetasAcep = async () => {
   try {
-    const response = await axios.get("src/data/emprendimientos.json");
+    const response = await axios.get(`${BASE_URL}/emprendimientos`);
     // Filtrar solo los emprendimientos con estado "aceptado"
     const aceptados = response.data.filter(emprendimiento => emprendimiento.estado === "aceptado");
     return aceptados;
@@ -13,21 +15,20 @@ export const getTarjetasAcep = async () => {
 
 export const getTarjetasPend = async () => {
   try {
-    const response = await axios.get("src/data/emprendimientos.json");
+    const response = await axios.get(`${BASE_URL}/emprendimientos`);
     // Filtrar solo los emprendimientos con estado "pendiente"
     const pendientes = response.data.filter(emprendimiento => emprendimiento.estado === "pendiente");
     return pendientes;
   } catch (error) {
     console.error("Error al cargar datos:", error);
   }
-}
+};
 
 export const getTarjeta = async (id) => {
   try {
-    const response = await axios.get("src/data/emprendimientos.json");
+    const response = await axios.get(`${BASE_URL}/emprendimientos`);
     return response.data.find((tarjeta) => tarjeta.id === id);
   } catch (error) {
     console.error("Error al cargar datos:", error);
   }
-}
-
+};
