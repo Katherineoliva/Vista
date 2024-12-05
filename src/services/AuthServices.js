5311ab7305460d8a60063c553f2a7f7a0e5c167e
Katherineoliva <104865553+Katherineoliva@users.noreply.github.com>
2024-12-04 23:00:33 -0600

    Update AuthServices.js

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'https://base-dusky-two.vercel.app/api';
 
 export const AuthConecction = async (data) => {
 
